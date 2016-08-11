d062cc8cbb6cb78972bc32c892464cf3a2d6f319
Lars Lehtonen <lars.lehtonen@gmail.com>
2016-08-10 22:49:55 -0700

    first pass, tested with mp3 collection.

---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/extemporalgenome/slug"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func copyToFat(src string, dest string) {
+	splitSrc, err := splitPath(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rootDepth := len(splitSrc)
+	walkFunc := func(path string, info os.FileInfo, err error) (localErr error) {
+		if !info.IsDir() {
+			splitPath, localErr := splitPath(path)
+			if localErr != nil {
+				return localErr
+			}
+			// Create a list to assemble the fully-qualified path of the destination.
+			var destList []string
+			// Start the list with the destination directory that was passed
+			// to the function.
+			destList = append(destList, dest)
+			// append all of the subdirectories from the source, not including the root
+			// directory or above as passed into the function, nor the filename.
+			// Strip characters that will confuse a FAT filesystem.
+			for _, e := range splitPath[rootDepth : len(splitPath)-1] {
+				destList = append(destList, slug.Slug(e))
+			}
+			// Slug the filename, retain the filename extension.
+			fileName, err := slugFileName(splitPath[len(splitPath)-1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			// Append the slugged filename to the list representing the destination
+			// directory.
+			destList = append(destList, fileName)
+			// Join the list into a string representing a path using the OS-specific
+			// path seperator.
+			destFQP := strings.Join(destList, string(os.PathSeparator))
+			// This looks like an error is being thrown away, but instead it means
+			// that in this case we're only interested in the filename, not the file.
+			destDir, _ := filepath.Split(destFQP)
+			_, err = os.Stat(destDir)
+			if os.IsNotExist(err) {
+				log.Printf("creating directory %s\n", destDir)
+				os.MkdirAll(destDir, 0777)
+			}
+			err = cp(path, destFQP)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("Copied %s to %s\n", path, destFQP)
+		}
+		return
+	}
+	filepath.Walk(src, walkFunc)
+}
+
+func cp(src string, dest string) error {
+	srcHandle, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcHandle.Close()
+	destHandle, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(destHandle, srcHandle); err != nil {
+		destHandle.Close()
+		return err
+	}
+	return destHandle.Close()
+}
+
+func slugFileName(fn string) (string, error) {
+	lastDot := strings.LastIndex(fn, ".")
+	beforeDot := slug.Slug(fn[:lastDot])
+	return beforeDot + fn[lastDot:], nil
+}
+
+func splitPath(path string) ([]string, error) {
+	if path[0] != os.PathSeparator {
+		return nil, fmt.Errorf("%s not a fully-qualified path", path)
+	}
+	cleanPath := filepath.Clean(string(path[1:]))
+	return strings.Split(cleanPath, string(os.PathSeparator)), nil
+}
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) < 2 {
+		log.Fatal("fatcp requires a source and a destination argument.")
+	}
+
+	srcDir, err := filepath.Abs(flag.Args()[0])
+	if err != nil {
+		log.Fatal("source directory is not a valid directory name.")
+	}
+	destDir, err := filepath.Abs(flag.Args()[1])
+	if err != nil {
+		log.Fatal("destination is not a valid directory name.")
+	}
+
+	srcStat, err := os.Stat(srcDir)
+	if os.IsNotExist(err) {
+		log.Fatal("source directory does not exist.")
+	}
+	if !srcStat.IsDir() {
+		log.Fatal("source is not a directory.")
+	}
+
+	copyToFat(srcDir, destDir)
+}
